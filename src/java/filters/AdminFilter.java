61328e737fdf707dd7d2361d35b9c62cbeb8cccc
emily <emily@Emilys-Book>
2021-07-16 00:09:44 -0600

    First Commit created the AdminFilter

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author emily
+ */
+public class AdminFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+    }
+
+    public void init(FilterConfig filterConfig) {
+
+    }
+}
