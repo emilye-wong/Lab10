ec822d1c807fb2644e50518a0691478a34ee91fe
emily <emily@Emilys-Book>
2021-07-17 01:29:27 -0600

    Second Commit created the redirects for regular user and unauthorized users

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -15,21 +11,54 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
 
-/**
- *
- * @author emily
- */
 public class AdminFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+        String email = (String) session.getAttribute("email");
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+
+        // authentication 
+        if (email == null || user == null) {
+            httpResponse.sendRedirect("login");
+            // always return on a redirect
+            return;
+        }
+
+        // regular user authentication
+        if (user.getRole().getRoleId() == 2) {
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+
+        // unauthorized user
+        if (user.getRole().getRoleId() != 1 && user.getRole().getRoleId() != 2) {
+            httpResponse.sendRedirect("login");
+            return;
+        }
+
+        // code before chain.doFilter will execute before the servlet
+        chain.doFilter(request, response); // execute the servlet, or the next filter in the chain
+        // code after chain.doFilter will execute after the servlet, during the response
+
     }
 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
     public void destroy() {
-    }
-
-    public void init(FilterConfig filterConfig) {
 
     }
 }
